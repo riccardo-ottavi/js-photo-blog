729a0cda7ccfa623543c3137a5c5a4b7b21eaddc
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-17 14:33:57 +0200

    mostra in pagina le immagini

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-console.log("prova");
+//seleziono i div delle immagini
+const picOut = document.querySelectorAll(".card-img");
+console.log(picOut);
+//inizializza endpoint
 const myEndPoint = "https://lanciweb.github.io/demo/api/pictures/";
 
-axios.get(myEndPoint)
-    .then(risposta => {
+
+
+//chiamata APIs
+
+axios.get(myEndPoint).then(risposta => {
         //successo
-        console.log("ciao 1")
+
+        //estrapolo i dati
+        let myApis = risposta.data;   
+        console.log("successo",myApis);
+        //mando in pagina i dati ricevuti
+        for (let i = 0; i < picOut.length; i++){
+            picOut[i].innerHTML = 
+                    `
+                        <img src="${myApis[i].url}" alt=""> 
+
+                    `
+        }
+        
+
+       
     })
     .catch(error => {
         console.error(error);
