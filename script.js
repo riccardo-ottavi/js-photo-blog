126772c28fcc49ea0c21bf52dced5c8b90c6e57a
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-17 19:21:52 +0200

    cattura l'indice della card su cui è il mouse

--- script.js
+++ script.js
@@ -10,57 +10,57 @@ const myEndPoint = "https://lanciweb.github.io/demo/api/pictures/";
 
 
 //----------main------------
-//chiamata APIs
+//chiamata API
 axios.get(myEndPoint).then(risposta => {
-        //successo
-        //estrapolo i dati
-        let myApis = risposta.data;   
-        console.log("successo",myApis);
-        //mando in pagina i dati ricevuti
-        displayData(myApis);
-    })
+    //successo
+    //estrapolo i dati
+    let myApis = risposta.data;
+    console.log("successo", myApis);
+    //mando in pagina i dati ricevuti
+    displayData(myApis);
+})
     .catch(error => {
         console.error(error);
     })
 
-//gestione hover
-cards.forEach(card=> {
-    card.addEventListener("mouseenter",
-    function(){
-    console.log("mouse entrato");  
-    });  
-});
 
-cards.forEach(card => {
-    card.addEventListener("mouseleave",
-    function(){
-    console.log("mouse uscito");  
-    });  
-});
- 
-
-
-
-
-
-    //manda in pagina gli oggetti passati 
-    function displayData(data){
-        for (let i = 0; i < picOut.length; i++){
-            //mando in pagina l'immagine
-            picOut[i].innerHTML = 
+//manda in pagina i dati ricevuti 
+function displayData(data) {
+    for (let i = 0; i < picOut.length; i++) {
+        //manda in pagina l'immagine
+        picOut[i].innerHTML =
+            `
+                        <img src="${data[i].url}" alt="" class="trip-pic"> 
                     `
-                        <img src="${data[i].url}" alt=""> 
-                    `
-            //mando in pagina il titolo        
-            titleOut[i].innerHTML =
-                    `
+
+        //manda in pagina il titolo        
+        titleOut[i].innerHTML =
+            `
                         <span>${data[i].title}</span> 
                     `
 
-            //mando in pagina il titolo        
-            dateOut[i].innerHTML =
-                    `
+        //manda in pagina il titolo        
+        dateOut[i].innerHTML =
+            `
                         <span>${data[i].date}</span> 
                     `
-        }   
     }
+    //seleziona le immagini appena create
+    const pics = document.querySelectorAll(".trip-pic");
+    //crea ed assegna gli event listener
+    for (let i = 0; i < pics.length; i++ ){
+        cards[i].addEventListener("mouseenter",
+             function () {
+                    console.log("mouse entrato su " + (i+1));
+            });
+    };
+
+    for (let i = 0; i < pics.length; i++ ){
+        cards[i].addEventListener("mouseleave",
+             function () {
+                    console.log("mouse uscito da " + (i+1));
+            });
+    };
+}
+
+
