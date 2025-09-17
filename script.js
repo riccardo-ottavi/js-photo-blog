bb99350c0a75a15dd013ccabd4faa8ae5d620cc7
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-17 12:41:57 +0200

    inizializza chiamata API

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("prova");
+const myEndPoint = "https://lanciweb.github.io/demo/api/pictures/";
+
+axios.get(myEndPoint)
+    .then(risposta => {
+        //successo
+        console.log("ciao 1")
+    })
+    .catch(error => {
+        console.error(error);
+    })
