4816af64f2951401a4a8aaffd9a5b5c92f7d80ca
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-17 19:45:38 +0200

    migliora leggibilità e commenta

--- script.js
+++ script.js
@@ -8,15 +8,14 @@ const myEndPoint = "https://lanciweb.github.io/demo/api/pictures/";
 
 
 //----------main------------
-
 //chiamata API
 axios.get(myEndPoint).then(risposta => {
-    //successo
     //estrapolo i dati
     let myApis = risposta.data;
     console.log("successo", myApis);
     //mando in pagina i dati ricevuti
     displayData(myApis);
+    //seleziono le pics appena create e creo gli eventi
     let pics = document.querySelectorAll(".trip-pic");
     eventHandler(pics);
 })
@@ -31,27 +30,17 @@ axios.get(myEndPoint).then(risposta => {
 function displayData(data) {
     for (let i = 0; i < picOut.length; i++) {
         //manda in pagina l'immagine
-        picOut[i].innerHTML =
-            `
-                        <img src="${data[i].url}" alt="" class="trip-pic"> 
-                    `
-
+        picOut[i].innerHTML = `<img src="${data[i].url}" alt="" class="trip-pic">`
         //manda in pagina il titolo        
-        titleOut[i].innerHTML =
-            `
-                        <span>${data[i].title}</span> 
-                    `
-
+        titleOut[i].innerHTML =`<span>${data[i].title}</span>`
         //manda in pagina il titolo        
-        dateOut[i].innerHTML =
-            `
-                        <span>${data[i].date}</span> 
-                    `
+        dateOut[i].innerHTML =`<span>${data[i].date}</span>`
     }
-    //crea ed assegna gli event listener
 }
 
+//aggiunge gli eventi per gestire l'hover 
 function eventHandler(element){
+    //non forEach perché serve salvare i
     for (let i = 0; i < element.length; i++ ){
         cards[i].addEventListener("mouseenter",
              function () {
